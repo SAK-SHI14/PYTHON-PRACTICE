baf151e77b3af1bbca0978dd3729c3438021ae54
SAKSHI VERMA <vermasakshi0514@gmail.com>
2024-06-06 22:51:21 +0530

    1

---
+++ 6 June.py
@@ -0,0 +1,30 @@
+# Type Code Here
+# FUnctions
+name = input("enter your name: ")
+print("hello, ", name)
+      
+      a=10
+      b=2
+      
+      print(a+b)
+      print(a-b)
+      print(a*b)
+      print(a/b)
+      print(a//b)
+      print(a%b)
+      print(A%b)
+      print(a**b)
+
+      print(a>2 and b<2)
+      print(a>5 or b<8)
+
+animals = ["panda", "elephant","lion"]
+ for animal in animals:
+    print(animal)
+
+
+    count = 0 
+    while count =<4:
+          print (count)
+          count +=1    
+
