24d4c8397b3338b96cff981b55b632a1dc448e22
SAKSHI VERMA <vermasakshi0514@gmail.com>
2024-06-07 13:52:38 +0530

    1

--- 7 june.py
+++ 7 june.py
@@ -17,3 +17,31 @@ print("motu is a {}".format(motu.__class__.attr1))
 
 print("my name is {}".format(kungfu.name))
 print("my name is {}".format(motu.name))
+
+class Base:
+    def __init__(self):
+        self.a = "futurense"
+        self.__c = "futurense"
+
+class Derived(Base):
+    def __init__(self):
+        super().__init__()
+        print("calling private number of base class: ")
+       
+
+obj1 = Base()
+print(obj1.a)  
+
+obj2 = Derived()
+
+
+class Person:
+    def __init__(self, name, age):
+        self.__name = name  
+        self.__age = age
+
+    def get_name(self):
+        return self.__name
+
+person = Person("sakshi", 30)
+print(person.get_name())  # John
